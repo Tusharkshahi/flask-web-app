bd20d8b90ad26712cd98f9d4c9a89d2ecac4f01a
Tushar Kumar Shahi <tusharkshahi@gmil.com>
2024-01-16 17:28:53 +0530

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Tushar!'
+    return 'Hello, Set Your heart ablaze!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
